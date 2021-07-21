303bf52d334cb4cefa9aa648ba887db0c6164164
MRyser <rysermariano@gmail.com>
2021-07-21 10:32:27 +0200

    Proyecto Pequenio php

---
+++ src/ex-01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css.css/css.css">
+</head>
+
+<body>
+    <h1>Zeichenketten verbinden in php (String concatenation)</h1>
+    <?php 
+
+    $name = 'Peter';
+    $str = 'Ich heisse ' . $name . '.';
+
+    echo "\$str = 'Ich heisse ' . \$name . '.'" . '<br>';
+    echo $str . '<br>';
+    
+    ?>
+</body>
+
+</html>
--- src/projects/quizMariano/info.php
+++
@@ -1,8 +0,0 @@
-<?php
-$projData = [
-    //"ProjectName" => '<img src="../../images/axle.jpg"><div>Quiz for dummies </div></img>',
-    "ProjectName" => 'Quiz PHP DAM',
-    "Author" => "Mariano Ryser",
-    "Date" => "01.07.2021",
-    "StartPage" => "index.php"
-];
